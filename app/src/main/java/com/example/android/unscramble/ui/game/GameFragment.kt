0ed6792822c85a82bd91d5b0d91d1c933e2d8688
Wanderson <lopez.wanderson@gmail.com>
2022-06-20 14:50:45 -0600

    Se implementa la función onSubmitWord()

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -74,7 +74,11 @@ class GameFragment : Fragment() {
     * Displays the next scrambled word.
     */
     private fun onSubmitWord() {
-
+        if (viewModel.nextWord()) {
+            updateNextWordOnScreen()
+        } else {
+            showFinalScoreDialog()
+        }
     }
 
     /*
