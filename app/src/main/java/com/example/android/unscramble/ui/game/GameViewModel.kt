8fff4913a960a2814af7d011ee19c3973aa5e9ca
Wanderson <lopez.wanderson@gmail.com>
2022-06-20 16:15:59 -0600

    Ordenamiento y comentarios

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -8,10 +8,18 @@ class GameViewModel: ViewModel() {
     private var _score = 0
     val score: Int
         get() = _score
+
     private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
+
     private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
+
+    // List of words used in the game
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
 
     init {
         Log.d("GameFragment", "GameViewModel created!")
@@ -22,9 +30,6 @@ class GameViewModel: ViewModel() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
     }
-
-    private var wordsList: MutableList<String> = mutableListOf()
-    private lateinit var currentWord: String
 
     private fun getNextWord() {
         currentWord = allWordsList.random()
